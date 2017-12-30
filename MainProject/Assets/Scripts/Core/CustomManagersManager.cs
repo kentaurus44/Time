be9acb9d22a5aef8e0fd03c2a9ecf4336cf27901
kentaurus <rigil44@gmail.com>
2017-12-29 21:04:12 -0500

    Created flow of Game how it goes into the game.

---
+++ MainProject/Assets/Scripts/Core/CustomManagersManager.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CustomManagersManager : ManagersManager
+{
+    [SerializeField]
+    protected TextAsset _eventsJSON;
+
+    [SerializeField]
+    protected TextAsset _dialogsJSON;
+
+    public override IEnumerator Load()
+    {
+        yield return base.Load();
+
+        GameManager.Instance.Load(_eventsJSON.ToString(), _dialogsJSON.ToString());
+    }
+}
