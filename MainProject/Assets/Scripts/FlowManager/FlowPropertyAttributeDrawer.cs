002da59607c4c886bd1bf2e8b62dbb793b8294a4
kentaurus <rigil44@gmail.com>
2018-03-10 21:45:51 -0500

    Updates

--- MainProject/Assets/Scripts/FlowManager/FlowPropertyAttributeDrawer.cs
+++ MainProject/Assets/Scripts/FlowManager/FlowPropertyAttributeDrawer.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections;
+﻿#if UNITY_EDITOR
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEditor;
@@ -35,3 +36,4 @@ public class FlowAttributeDrawer : PropertyDrawer
 		EditorGUI.EndProperty();
 	}
 }
+#endif
--- MainProject/Assets/Scripts/FlowManager/FlowView.cs
+++ MainProject/Assets/Scripts/FlowManager/FlowView.cs
@@ -6,8 +6,6 @@ using UnityEngine;
 public class FlowView : ScriptableObject
 {
 	[SerializeField]
-	private string _id;
-	[SerializeField]
 	private bool _isPopup = false;
 
 	[Header("Loading Screen")]
@@ -16,7 +14,7 @@ public class FlowView : ScriptableObject
 	[SerializeField]
 	private bool _onEnter = false;
 
-	public string Id { get { return _id; } }
+	public string Id { get { return name; } }
 	public bool IsPopup { get { return _isPopup; } }
 	public bool UseLoadingEnter { get { return _onEnter; } }
 	public bool UseLoadingExit { get { return _onExit; } }
